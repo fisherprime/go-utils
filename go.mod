ed1b95fccee6cbbe69f2c74f965b8491307623b5
fisherprime <mbock.ernie40@gmail.com>
2024-06-11 16:24:18 +0300

    chore: Deprecate logrus

--- go.mod
+++ go.mod
@@ -1,10 +1,3 @@
 module gitlab.com/fisherprime/go-utils
 
 go 1.20
-
-require github.com/sirupsen/logrus v1.8.1
-
-require (
-	github.com/stretchr/testify v1.5.1 // indirect
-	golang.org/x/sys v0.0.0-20220204135822-1c1b9b1eba6a // indirect
-)
--- types/types.go
+++ types/types.go
@@ -5,8 +5,6 @@ package types
 
 import (
 	"errors"
-
-	"github.com/sirupsen/logrus"
 )
 
 // Misc constants.
@@ -14,13 +12,7 @@ const (
 	UndefinedUint uint = 0
 )
 
-// fLogger is a logrus.FieldLogger used for debug purposes.
-var fLogger logrus.FieldLogger = logrus.NewEntry(logrus.New())
-
 // Validation errors.
 var (
 	ErrInvalidIndex = errors.New("invalid index")
 )
-
-// SetLogger sets the debugging logrus.FieldLogger.
-func SetLogger(l *logrus.Entry) { fLogger = l }
